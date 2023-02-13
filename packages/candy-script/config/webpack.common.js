5b393baf6a2f139386b0068ff94eb9637b268004
xun <2042204285@qq.com>
2023-02-13 14:12:58 +0800

    fix: 小修小补,增加用户自定义webpack权重

--- packages/candy-script/config/webpack.common.js
+++ packages/candy-script/config/webpack.common.js
@@ -11,160 +11,164 @@ const stylelint = require("stylelint");
 const { merge } = require("webpack-merge");
 const isDevelopment = process.argv.slice(2)[0] === "serve";
 
-const userWebpackConfig = require(resolveApp("./react.config.js"));
+const userWebpackConfig = require(resolveApp("./webpack.config.js"));
 
-module.exports = merge(userWebpackConfig, {
-  stats: "errors-only",
-  entry: resolveApp("./src/index.js"),
-  output: {
-    path: isDevelopment ? undefined : resolveApp("./dist"),
-    // 图片、字体资源
-    assetModuleFilename: "assets/[hash][ext][query]",
-    filename: isDevelopment
-      ? "static/js/[name].bundle.js"
-      : "static/js/[name].[contenthash:8].bundle.js",
-    // 动态导入资源
-    chunkFilename: isDevelopment
-      ? "static/js/[name].chunk.js"
-      : "static/js/[name].[contenthash:8].chunk.js",
-    // webpack5内置了 clean-webpack-plugin,只需将 clean 设置为 true
-    clean: true,
-    pathinfo: false, // 关闭 Webpack 在输出的 bundle 中生成路径信息
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$|\.scss$|\.less$/i,
-        include: [resolveApp("./src")],
-        exclude: /node_module/,
-        use: [
-          isDevelopment
-            ? require.resolve("style-loader")
-            : MiniCssExtractPlugin.loader,
-          {
-            loader: require.resolve("css-loader"),
-            options: {
-              sourceMap: isDevelopment, // 在开发环境下开启
-              modules: {
-                mode: "local",
-                auto: true,
-                exportGlobals: true,
-                localIdentName: isDevelopment
-                  ? "[path][name]__[local]--[hash:base64:5]"
-                  : "[local]--[hash:base64:5]",
-                localIdentContext: resolveApp("./src"),
-                exportLocalsConvention: "camelCase",
+module.exports = merge(
+  {
+    stats: "errors-only",
+    entry: resolveApp("./src/index.js"),
+    output: {
+      path: isDevelopment ? undefined : resolveApp("./dist"),
+      // 图片、字体资源
+      assetModuleFilename: "assets/[hash][ext][query]",
+      filename: isDevelopment
+        ? "static/js/[name].bundle.js"
+        : "static/js/[name].[contenthash:8].bundle.js",
+      // 动态导入资源
+      chunkFilename: isDevelopment
+        ? "static/js/[name].chunk.js"
+        : "static/js/[name].[contenthash:8].chunk.js",
+      // webpack5内置了 clean-webpack-plugin,只需将 clean 设置为 true
+      clean: true,
+      pathinfo: false, // 关闭 Webpack 在输出的 bundle 中生成路径信息
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$|\.scss$|\.less$/i,
+          include: [resolveApp("./src")],
+          exclude: /node_module/,
+          use: [
+            isDevelopment
+              ? require.resolve("style-loader")
+              : MiniCssExtractPlugin.loader,
+            {
+              loader: require.resolve("css-loader"),
+              options: {
+                sourceMap: isDevelopment, // 在开发环境下开启
+                modules: {
+                  mode: "local",
+                  auto: true,
+                  exportGlobals: true,
+                  localIdentName: isDevelopment
+                    ? "[path][name]__[local]--[hash:base64:5]"
+                    : "[local]--[hash:base64:5]",
+                  localIdentContext: resolveApp("./src"),
+                  exportLocalsConvention: "camelCase",
+                },
+                importLoaders: 2,
               },
-              importLoaders: 2,
             },
-          },
-          {
-            loader: require.resolve("postcss-loader"),
-            options: {
-              postcssOptions: {
-                plugins: [
-                  require("autoprefixer"),
-                  stylelint({
-                    config: {
-                      rules: {
-                        "declaration-no-important": true,
+            {
+              loader: require.resolve("postcss-loader"),
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    require("autoprefixer"),
+                    stylelint({
+                      config: {
+                        rules: {
+                          "declaration-no-important": true,
+                        },
                       },
-                    },
-                  }),
-                ],
+                    }),
+                  ],
+                },
               },
             },
-          },
-          useCssPreset("sass") && require.resolve("sass-loader"),
-          useCssPreset("less") && require.resolve("less-loader"),
-        ].filter(Boolean),
-      },
-      // 处理图片
-      {
-        test: /\.(jpe?g|png|gif|webp|svg|mp4)$/,
-        type: "asset",
-        // 文件生成路径
-        generator: {
-          filename: "./images/[hash:8][ext][query]",
-        },
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb会被压缩成base64
-          },
-        },
-      },
-      // 处理字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "./assets/fonts/[hash][ext][query]",
-        },
-      },
-      // 处理js
-      {
-        test: /\.(tsx?|jsx?)$/,
-        include: [resolveApp("./src")],
-        loader: require.resolve("babel-loader"),
-        options: {
-          babelrc: true,
-          cacheDirectory: true,
-          cacheCompression: false, // 缓存不压缩
-          presets: [
-            require.resolve("@babel/preset-react"),
-            isUseTypescript && require.resolve("@babel/preset-typescript"),
-            [
-              require.resolve("@babel/preset-env"),
-              {
-                useBuiltIns: "usage", // 代码中需要那些polyfill,就引用相关的api
-                corejs: 3, // 配置使用core-js低版本
-              },
-            ],
+            useCssPreset("sass") && require.resolve("sass-loader"),
+            useCssPreset("less") && require.resolve("less-loader"),
           ].filter(Boolean),
-          plugins: [
-            isDevelopment && require.resolve("react-refresh/babel"), // 激活 js 的 HMR
-            require.resolve("@babel/plugin-transform-runtime"),
+        },
+        // 处理图片
+        {
+          test: /\.(jpe?g|png|gif|webp|svg|mp4)$/,
+          type: "asset",
+          // 文件生成路径
+          generator: {
+            filename: "./images/[hash:8][ext][query]",
+          },
+          parser: {
+            dataUrlCondition: {
+              maxSize: 10 * 1024, // 小于10kb会被压缩成base64
+            },
+          },
+        },
+        // 处理字体
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "./assets/fonts/[hash][ext][query]",
+          },
+        },
+        // 处理js
+        {
+          test: /\.(tsx?|jsx?)$/,
+          include: [resolveApp("./src")],
+          loader: require.resolve("babel-loader"),
+          options: {
+            babelrc: true,
+            cacheDirectory: true,
+            cacheCompression: false, // 缓存不压缩
+            presets: [
+              require.resolve("@babel/preset-react"),
+              isUseTypescript && require.resolve("@babel/preset-typescript"),
+              [
+                require.resolve("@babel/preset-env"),
+                {
+                  useBuiltIns: "usage", // 代码中需要那些polyfill,就引用相关的api
+                  corejs: 3, // 配置使用core-js低版本
+                },
+              ],
+            ].filter(Boolean),
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"), // 激活 js 的 HMR
+              require.resolve("@babel/plugin-transform-runtime"),
 
-            require.resolve("@babel/plugin-syntax-dynamic-import"),
-          ].filter(Boolean),
+              require.resolve("@babel/plugin-syntax-dynamic-import"),
+            ].filter(Boolean),
+          },
+          exclude: [/node_modules/, /public/, /(.|_)min\.js$/],
         },
-        exclude: [/node_modules/, /public/, /(.|_)min\.js$/],
+      ].filter(Boolean),
+    },
+    resolve: {
+      extensions: [".js"],
+      alias: {
+        "@pages": resolveApp("scr/pages"),
+        "@": resolveApp("src"),
       },
+    },
+    plugins: [
+      new WebpackBar(),
+      new HtmlWebpackPlugin({
+        template: resolveApp("./public/index.html"),
+        filename: "index.html",
+        title: "moment",
+        inject: true,
+        hash: true,
+        minify: isDevelopment
+          ? false
+          : {
+              // https://github.com/terser/html-minifier-terser#options-quick-reference
+              removeComments: true, // 删除注释
+              collapseWhitespace: true, //是否去除空格
+              minifyCSS: true, // 压缩 HTML 中的 css 代码
+              minifyJS: true, // 压缩 HTML 中出现的 JS 代码
+              caseSensitive: true, // 区分大小写
+              removeRedundantAttributes: true, // 当值与默认值匹配时删除属性。
+              removeEmptyAttributes: true, // 删除所有只有空白值的属性
+              removeStyleLinkTypeAttributes: true, // 从样式和链接标签中删除type="text/css"。其他类型属性值保持不变
+              removeScriptTypeAttributes: true, // 从脚本标签中删除type="text/javascript"其他类型属性值保持不变
+              useShortDoctype: true, // 将文档类型替换为短(HTML5)文档类型
+            },
+      }),
+      // 定义全局常量
+      new DefinePlugin({
+        BASE_URL: '"./"',
+      }),
     ].filter(Boolean),
   },
-  resolve: {
-    extensions: [".js", ".json", ".jsx", ".ts", ".css", ".tsx"],
-    alias: {
-      "@": resolveApp("./scr"),
-    },
-  },
-  plugins: [
-    new WebpackBar(),
-    new HtmlWebpackPlugin({
-      template: resolveApp("./public/index.html"),
-      filename: "index.html",
-      title: "moment",
-      inject: true,
-      hash: true,
-      minify: isDevelopment
-        ? false
-        : {
-            // https://github.com/terser/html-minifier-terser#options-quick-reference
-            removeComments: true, // 删除注释
-            collapseWhitespace: true, //是否去除空格
-            minifyCSS: true, // 压缩 HTML 中的 css 代码
-            minifyJS: true, // 压缩 HTML 中出现的 JS 代码
-            caseSensitive: true, // 区分大小写
-            removeRedundantAttributes: true, // 当值与默认值匹配时删除属性。
-            removeEmptyAttributes: true, // 删除所有只有空白值的属性
-            removeStyleLinkTypeAttributes: true, // 从样式和链接标签中删除type="text/css"。其他类型属性值保持不变
-            removeScriptTypeAttributes: true, // 从脚本标签中删除type="text/javascript"其他类型属性值保持不变
-            useShortDoctype: true, // 将文档类型替换为短(HTML5)文档类型
-          },
-    }),
-    // 定义全局常量
-    new DefinePlugin({
-      BASE_URL: '"./"',
-    }),
-  ].filter(Boolean),
-});
+  userWebpackConfig
+);
--- packages/candy-script/config/webpack.dev.js
+++ packages/candy-script/config/webpack.dev.js
@@ -8,34 +8,37 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 
 const webpackCommonConfig = require("./webpack.common");
 
-module.exports = merge(webpackCommonConfig, {
-  mode: "development",
-  devtool: "inline-source-map",
-  plugins: [
-    new ESLintPlugin({
-      extensions: ["js", "mjs", "jsx", "ts", "tsx"],
-      eslintPath: require.resolve("eslint"),
-      context: resolveApp("./src"),
-      cwd: resolveApp("."),
-      cacheLocation: path.resolve(
-        resolveApp("node_modules"),
-        ".cache/.eslintcache"
-      ),
-    }),
-    // 还没有使用到,暂时不开启
-    // new ReactRefreshWebpackPlugin(),
-
-    isUseTypescript &&
-      new ForkTsCheckerWebpackPlugin({
-        async: false,
+module.exports = merge(
+  {
+    mode: "development",
+    devtool: "inline-source-map",
+    plugins: [
+      new ESLintPlugin({
+        extensions: ["js", "mjs", "jsx", "ts", "tsx"],
+        eslintPath: require.resolve("eslint"),
+        context: resolveApp("./src"),
+        cwd: resolveApp("."),
+        cacheLocation: path.resolve(
+          resolveApp("node_modules"),
+          ".cache/.eslintcache"
+        ),
       }),
-    //  解决模块循环引入问题
-    new CircularDependencyPlugin({
-      exclude: /node_modules/,
-      include: /src/,
-      failOnError: true,
-      allowAsyncCycles: false,
-      cwd: process.cwd(),
-    }),
-  ].filter(Boolean),
-});
+      // 还没有使用到,暂时不开启
+      // new ReactRefreshWebpackPlugin(),
+
+      isUseTypescript &&
+        new ForkTsCheckerWebpackPlugin({
+          async: false,
+        }),
+      //  解决模块循环引入问题
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+        include: /src/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      }),
+    ].filter(Boolean),
+  },
+  webpackCommonConfig
+);
