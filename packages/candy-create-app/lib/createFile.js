ebb6651e59661f3663c850a10a4ec046f68c6eca
xun <2042204285@qq.com>
2023-02-13 09:40:08 +0800

    fix: 修复创建文件参数存在问题的情况

--- packages/candy-create-app/lib/createFile.js
+++ packages/candy-create-app/lib/createFile.js
@@ -6,7 +6,7 @@ import { mkdirFileName } from "./questions.js";
 import { simplifyPath, fileTypeRouter } from "./index.js";
 import { removeExitMatter } from "./questions.js";
 
-export default async function createFile(type, options = ".") {
+export default async function createFile(type, options) {
   const { filename } = await inquirer.prompt(mkdirFileName);
   const reg = /^[a-z]+$/;
   if (!reg.test(filename)) {
@@ -14,23 +14,25 @@ export default async function createFile(type, options = ".") {
     process.exit(1);
   }
 
-  const useLanguage = isUseTypescript ? "t" : "J";
+  const useLanguage = isUseTypescript ? "t" : "j";
   // 简化路径,以免用户睡着了输错路径
-  const router = simplifyPath(options);
+  const router = options && simplifyPath(options);
   const defaultRouter = fileTypeRouter(useLanguage, filename).get(type);
   const suffix = defaultRouter.split(".").at(-1);
   const userRouter = `${router}/${filename}.${suffix}`;
 
   // 完整路径,如果用户不输入路径则使用默认路径
-  const fileRouter = resolveApp(options === "." ? defaultRouter : userRouter);
+  const fileRouter = resolveApp(options ? userRouter : defaultRouter);
 
-  if (fileRouter) {
+  if (fs.existsSync(fileRouter)) {
     const { action } = await inquirer.prompt(
       removeExitMatter(fileRouter, "file")
     );
 
     if (action === true) fs.promises.unlink(fileRouter);
   }
+
+  console.log(fileRouter);
 }
 
 // 自动导入reducer
