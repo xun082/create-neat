8d39a06ae3165d6b89f00b273b8474f8de0868c0
“邓浩” <1051766345@qq.com>
2024-03-17 20:22:05 +0800

    feat: add devDependencies install:(#40)

--- packages/core/src/utils/dependenciesInstall.ts
+++ packages/core/src/utils/dependenciesInstall.ts
@@ -1,12 +1,13 @@
 import fs from "fs";
 import { spawn } from "child_process";
 import path from "path";
+
 /**
- *
+ *  安装package.json中的devDependencies依赖
  * @param packageJsonFile packageJson文件父路径
  * @param packageManager  包管理器
  */
-const dependenciesInstall = (packageJsonFile: string, packageManager: string) => {
+const dependenciesInstall = (packageJsonFile: string, packageManager: string): Promise<string> => {
   /** 不同安装方式的命令枚举  */
   const installCommand = {
     npm: "install",
@@ -14,6 +15,7 @@ const dependenciesInstall = (packageJsonFile: string, packageManager: string) =>
     yarn: "add",
   };
 
+  /** 不同安装方式的参数枚举 */
   const installParams = {
     npm: "--save-dev",
     pnpm: "--save-dev",
@@ -29,9 +31,11 @@ const dependenciesInstall = (packageJsonFile: string, packageManager: string) =>
 
       if (devDependencies) {
         console.log("Installing devDependencies...");
+        // 将devDependencies对象转换为数组，然后拼接成安装命令的字符串
         const devDepsArray = Object.entries(devDependencies).map(
           ([dep, version]) => `${dep}@${version}`,
         );
+        // 执行具体命令
         const pm = spawn(
           packageManager,
           [installCommand[packageManager], installParams[packageManager], ...devDepsArray],
@@ -40,8 +44,11 @@ const dependenciesInstall = (packageJsonFile: string, packageManager: string) =>
             cwd: packageJsonFile,
           },
         );
+
+        // 监听安装命令的输出
         pm.on("close", (code) => {
           if (code === 0) {
+            // code为0代表安装成功
             resolve("devDependencies installed successfully.");
             console.log("devDependencies installed successfully.");
           } else {
