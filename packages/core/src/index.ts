f2be23f1ae686199b9484b03542b92985cf21fff
xun082 <2042204285@qq.com>
2024-03-18 19:37:13 +0800

    fix: 🧩 修复 ci 配置文件错误导致的 ci 失败

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,31 +4,32 @@ import { Command } from "commander";
 import chalk from "chalk";
 import minimist from "minimist";
 
-import { getPackageJsonInfo, createApp } from "./utils";
+import { getPackageJsonInfo } from "./utils";
 import createAppTest from "./utils/createAppTest";
 
 const program = new Command();
 
 program.version(chalk.greenBright(getPackageJsonInfo("../../package.json", true).version));
 
-
 program
-  .command('create <project-name>')
-  .description('Create a directory for your project files')
-  .option('-f, --force', 'Overwrite target directory if it exists')
+  .command("create <project-name>")
+  .description("Create a directory for your project files")
+  .option("-f, --force", "Overwrite target directory if it exists")
   .action((name, options) => {
     // createApp(name, options);
     createAppTest(name, options);
   });
 
 program
-  .command('add <plugin> [pluginOptions]')
-  .description('Install a plugin and invoke its generator in an existing project')
-  .option('--registry <url>', 'Specify an npm registry URL for installing dependencies (npm only)')
+  .command("add <plugin> [pluginOptions]")
+  .description("Install a plugin and invoke its generator in an existing project")
+  .option("--registry <url>", "Specify an npm registry URL for installing dependencies (npm only)")
   .allowUnknownOption()
   .action((plugin) => {
     const pluginOptions = minimist(process.argv.slice(3));
+    console.log(pluginOptions, plugin);
+
     // addPlugin(plugin, pluginOptions)
   });
 
-program.parse(process.argv)
+program.parse(process.argv);
