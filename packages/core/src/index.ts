0d8878f2d098c15f44ec21cad461a97aead633b7
Sphinx-l <87009423+Sphinx-l@users.noreply.github.com>
2024-03-18 19:07:26 +0800

    feat: 基于 preset 生成中英文的 readme 文档: (#43) ---------
    Co-authored-by: uaenaTzx <3131496501@qq.com>
    Co-authored-by: liujunqi <liujunqi@vandream.com>
    (cherry picked from commit 26947c1a1fb3f9ff69d5392c7e6ebaf059f6832b)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,7 +4,7 @@ import { Command } from "commander";
 import chalk from "chalk";
 import minimist from "minimist";
 
-import { getPackageJsonInfo } from "./utils";
+import { getPackageJsonInfo, createApp } from "./utils";
 import createAppTest from "./utils/createAppTest";
 
 const program = new Command();
@@ -20,9 +20,9 @@ program
   });
 
 program
-  .command("add <plugin> [pluginOptions]")
-  .description("Install a plugin and invoke its generator in an existing project")
-  .option("--registry <url>", "Specify an npm registry URL for installing dependencies (npm only)")
+  .command('add <plugin> [pluginOptions]')
+  .description('Install a plugin and invoke its generator in an existing project')
+  .option('--registry <url>', 'Specify an npm registry URL for installing dependencies (npm only)')
   .allowUnknownOption()
   .action((plugin) => {
     const pluginOptions = minimist(process.argv.slice(3));
@@ -30,4 +30,4 @@ program
     console.log(plugin, pluginOptions);
   });
 
-program.parse(process.argv);
+program.parse(process.argv)
