2d88660ebb60851d81ce76f5f4705820cd9a803c
小朱 <103256281+Sunrisies@users.noreply.github.com>
2024-03-23 23:50:31 +0800

    feat: 添加npm源的选择 (#52) (#70)

--- packages/core/src/utils/preset.ts
+++ packages/core/src/utils/preset.ts
@@ -5,6 +5,7 @@ interface Preset {
   buildTool: string;
   plugins: Record<string, any>;
   packageManager: string;
+  npmSource: string;
 }
 
 /**
@@ -12,6 +13,7 @@ interface Preset {
  * @param template 模板
  * @param buildTool 构建工具
  * @param plugins 插件列表
+ * @param npmSource npm源
  * @return 用户预设
  */
 // todo: 目前没有使用到 defaultPreset，后续考虑加入默认配置
@@ -20,12 +22,14 @@ const getPreset = (
   buildTool: string,
   plugins: string[],
   packageManager: string,
+  npmSource: string,
 ): Preset => {
   const preset: Preset = {
     template,
     buildTool,
     plugins: {},
     packageManager,
+    npmSource,
   };
 
   // todo: 插件配置目前设置为空，且没有使用情况，后续优化
@@ -43,6 +47,7 @@ const defaultPreset: Preset = {
     eslint: {},
   },
   packageManager: "npm",
+  npmSource: "",
   // todo: 更多配置随构建需要添加
 };
 
