976aa8a62349b9ca11d79722175023132b5617d5
白帝子 <address>
2024-04-01 23:23:58 +0800

    feat: 遍历所有 commit

--- scripts/check_commit_msg.sh
+++ scripts/check_commit_msg.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
-
-# 获取待检查的提交信息
-commit_msg=$(cat $1)
+#!/bin/bash
 
 # 设置颜色变量
 RED='\033[0;31m'
@@ -11,14 +8,28 @@ NC='\033[0m' # No Color
 # 从commitlint.config.js导入rules变量
 rules=$(node -e "console.log(require('./commitlint.config.js').rules['type-enum'][2].join('|'))")
 
-# 检查提交信息是否以指定的前缀开头
-if ! echo "$commit_msg" | grep -qE "^($rules):"; then
-    echo -e "${RED}Error:${NC} Commit message format is incorrect. It should start with one of '${BLUE}$rules:${NC}'." >&2
-    exit 1
-fi
+# 定义提交信息规范函数
+check_commit_message() {
+    commit_msg="$1"
+    # 检查提交信息是否以指定的前缀开头
+    if ! echo "$commit_msg" | grep -qE "^($rules):"; then
+        echo -e "${RED}Error:${NC} Commit message format is incorrect for the following message:" >&2
+        echo "$commit_msg" >&2
+        return 1  # 返回非零状态以表示检查失败
+    fi
+}
+
+# 获取本地尚未推送的提交SHA值
+commit_sha_list=$(git log origin/main..HEAD --format="%H")
+
+# 遍历所有尚未推送的提交，并检查提交消息格式
+for sha in $commit_sha_list; do
+    commit_msg=$(git show --format=%B -s $sha)
+    check_commit_message "$commit_msg"
+    if [ $? -ne 0 ]; then
+        exit 1  # 如果消息检查失败，退出循环并终止脚本
+    fi
+done
 
 # 输出成功校验
 echo -e "${BLUE}Commit message check passed.${NC}\n"
-
-# 如果没有错误，则允许提交
-exit 0
