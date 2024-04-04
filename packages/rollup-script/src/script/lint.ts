27b6fc3c8fd380d9798058bbec91f67928c3f3cb
Cpeng888 <134287700+Cpeng888@users.noreply.github.com>
2024-04-04 21:07:45 +0800

    ci : 添加tsc检查--添加build命令 (#89)
    * ci: add a new workflow
    
    * ci: 添加tsc检查--添加build命令
    
    ---------
    
    Co-authored-by: CP <p.chen@synovate.tech>
    (cherry picked from commit 2c6e3c07cefb80231db5f6fc1064765f8fadba4e)

--- packages/rollup-script/src/script/lint.ts
+++
@@ -1,6 +0,0 @@
-import { LintParams } from "../types";
-import formatCode from "../config/prettierFormat";
-
-export async function lintProject(_options: LintParams): Promise<void> {
-  await formatCode();
-}
