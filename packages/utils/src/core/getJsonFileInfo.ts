b425cc37afa5dc208d57398592c4509e89a185e6
Kong <kak584754109@163.com>
2024-04-06 16:15:00 +0800

    feat: add the jsdoc of utils

--- packages/utils/src/core/getJsonFileInfo.ts
+++ packages/utils/src/core/getJsonFileInfo.ts
@@ -1,5 +1,10 @@
 import { readFileSync, existsSync } from "fs";
 
+/**
+ * 获取 JSON 文件信息。
+ * @param router JSON 文件路径。
+ * @returns JSON 文件的内容。
+ */
 export function getJsonFileInfo(router: string) {
   if (existsSync(router)) return JSON.parse(readFileSync(router).toString());
 }
