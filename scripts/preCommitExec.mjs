3290f7e5dc06949a9ec418b202b24b5f8c9a8ce4
Eadweard <804875728@qq.com>
2024-04-11 14:06:07 +0800

    refactor: ♻️ refactor logic and add precommit check

--- scripts/preCommitExec.mjs
+++ scripts/preCommitExec.mjs
@@ -13,27 +13,37 @@ function main() {
  * pre-commit hook to copy the root README.md to the target package.
  */
 function copyRootReadmeToTarget() {
-  try {
-    const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-    const __dirname = path.dirname(__filename);
-    const mark = "README.md";
-    const targetReadmePath = resolve(__dirname, `../packages/core/${mark}`);
-    const rootReadmePath = resolve(__dirname, `../${mark}`);
-    console.log(targetReadmePath)
-    console.log(rootReadmePath)
-    // 检查readme.md是否在暂存的文件中
-    const hasReadmeChanged = () => {
-      const gitDiff = execSync("git diff --cached --name-only", { encoding: "utf-8" });
-      return gitDiff.split("\n").some((file) => file.trim() === mark);
-    };
-    // 如果暂存区有readme.md文件，则将根目录的readme.md文件复制到暂存区
-    if (hasReadmeChanged()) {
-      execSync(`cp ${rootReadmePath} ${targetReadmePath}`);
-      console.log(`Copied root README.md to ${targetReadmePath}.`);
+  const rootReadmeMark = "README.md";
+
+  const rootReadmePath = resolve(process.cwd(), rootReadmeMark);
+  const targetFiles = ["packages/core/README.md"];
+  const cpFile = (targetPath) => {
+    if (hasFileChanged(rootReadmeMark)) {
+      execSync(`cp ${rootReadmePath} ${targetPath}`);
+      console.log(`Copied root README.md to ${targetPath}.`);
     }
+  }
+
+  try {
+    mapFilesToPath(targetFiles).forEach(cpFile);
   }
   catch (error) {
     console.error("Error executing git diff:", error);
     process.exit(1);
   }
+}
+
+
+function mapFilesToPath(files) {
+  return files.map((file) => resolve(process.cwd(), file));
+}
+
+/**
+ * Check if the file has changed in the git diff.
+ * @param path {string} The path to the file to check.
+ * @returns {boolean}
+ */
+function hasFileChanged(path) {
+  const gitDiff = execSync("git diff --cached --name-only", { encoding: "utf-8" });
+  return gitDiff.split("\n").some((file) => file.trim() === path);
 }
