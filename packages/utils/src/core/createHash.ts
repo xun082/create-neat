9bc918f52f43f513f303f3c403f05bdba3b3210b
KongAKun <143328179+KongAKun@users.noreply.github.com>
2024-04-11 17:44:09 +0800

    feat: add jsdoc of utils (#114)
    * feat: add jsdoc of utils
    
    * feat: add jsdoc of utils
    
    (cherry picked from commit 21cf3b68de7cfe6d405939505a59dd62e2ad473d)

--- packages/utils/src/core/createHash.ts
+++ packages/utils/src/core/createHash.ts
@@ -4,6 +4,11 @@ interface envConfig {
   [props: string]: string | number;
 }
 
+/**
+ * @description 创建基于环境配置的哈希值。
+ * @param env 环境配置对象。
+ * @returns 表示环境配置哈希的十六进制字符串。
+ */
 function createEnvironmentHash(env: envConfig) {
   const hash = createHash("md5");
   hash.update(JSON.stringify(env));
