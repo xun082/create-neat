568ee866b3b3237f189747efbeff49df32248506
hao deng <38180313+qq1051766345@users.noreply.github.com>
2024-04-21 14:18:02 +0800

    Revert "feat: 🚀 增加dependencies的安装方式"

--- packages/core/src/utils/dependenciesInstall.ts
+++ packages/core/src/utils/dependenciesInstall.ts
@@ -2,51 +2,44 @@ import fs from "fs";
 import { spawn } from "cross-spawn";
 import path from "path";
 
-/** 不同安装方式的命令枚举  */
-const installCommand = {
-  npm: "install",
-  pnpm: "add",
-  yarn: "add",
-};
-
-/** 不同安装方式的参数枚举 */
-const installParams = {
-  npm: "--save-dev",
-  pnpm: "--save-dev",
-  yarn: "--dev",
-};
-
 /**
- * 核心安装方法
- * @param dependencies 依赖对象
- * @param packageJsonFile package.json文件路径
- * @param packageManager 包管理器
- * @param isDev 是否为开发依赖
+ *  安装package.json中的devDependencies依赖
+ * @param packageJsonFile packageJson文件父路径
+ * @param packageManager  包管理器
  */
-const installDependencies = (
-  dependencies: any,
-  packageJsonFile: string,
-  packageManager: string,
-  isDev = true,
-): Promise<string> => {
+const dependenciesInstall = (packageJsonFile: string, packageManager: string): Promise<string> => {
+  /** 不同安装方式的命令枚举  */
+  const installCommand = {
+    npm: "install",
+    pnpm: "add",
+    yarn: "add",
+  };
+
+  /** 不同安装方式的参数枚举 */
+  const installParams = {
+    npm: "--save-dev",
+    pnpm: "--save-dev",
+    yarn: "--dev",
+  };
+
+  const packageJsonPath = path.join(packageJsonFile, "package.json");
+
   return new Promise((resolve, reject) => {
-    const dependenciesName = isDev ? "devDependencies" : "dependencies";
     try {
-      if (dependencies) {
-        console.log(`Installing ${dependenciesName}...`);
-        // 将dependencies对象转换为数组，然后拼接成安装命令的字符串
-        const devDepsArray = Object.entries(dependencies).map(
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+      const devDependencies = packageJson.devDependencies;
+
+      if (devDependencies) {
+        console.log("Installing devDependencies...");
+        // 将devDependencies对象转换为数组，然后拼接成安装命令的字符串
+        const devDepsArray = Object.entries(devDependencies).map(
           ([dep, version]) => `${dep}@${version}`,
         );
-
-        // 判断是否为开发依赖，如果是则添加参数
-        const params = isDev ? installParams[packageManager] : "";
-
         // 执行具体命令
         try {
           const pm = spawn(
             packageManager,
-            [installCommand[packageManager], params, ...devDepsArray],
+            [installCommand[packageManager], installParams[packageManager], ...devDepsArray],
             {
               stdio: "ignore",
               cwd: packageJsonFile,
@@ -57,8 +50,8 @@ const installDependencies = (
           pm.on("close", (code) => {
             if (code === 0) {
               // code为0代表安装成功
-              resolve(`${dependenciesName} installed successfully.`);
-              console.log(`${dependenciesName} installed successfully.`);
+              resolve("devDependencies installed successfully.");
+              console.log("devDependencies installed successfully.");
             } else {
               console.error(
                 `${packageManager} ${installCommand[packageManager]} exited with code ${code}`,
@@ -69,39 +62,17 @@ const installDependencies = (
             }
           });
         } catch (err) {
-          console.log(`Installing ${dependenciesName} failed: `, err);
+          console.log("Installing devDependencies failed: ", err);
         }
       } else {
-        console.log(`No ${dependenciesName} found in package.json.`);
-        // 如果没有依赖，则直接返回
-        resolve(`No ${dependenciesName} found in package.json.`);
+        console.log("No devDependencies found in package.json.");
+        reject("No devDependencies found in package.json.");
       }
     } catch (error) {
       console.error("Error reading package.json:", error);
       reject("Error reading package.json");
     }
   });
-};
-
-/**
- *  安装package.json中的devDependencies依赖
- * @param packageJsonFile packageJson文件父路径
- * @param packageManager  包管理器
- */
-const dependenciesInstall = async (
-  packageJsonFile: string,
-  packageManager: string,
-): Promise<string[]> => {
-  const packageJsonPath = path.join(packageJsonFile, "package.json");
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
-  const devDependenciesObj = packageJson.devDependencies;
-  const dependenciesObj = packageJson.dependencies;
-
-  // 拿到具体的依赖对象，然后执行安装
-  const devPromise = installDependencies(devDependenciesObj, packageJsonFile, packageManager);
-  const promise = installDependencies(dependenciesObj, packageJsonFile, packageManager);
-
-  return await Promise.all([devPromise, promise]);
 };
 
 export default dependenciesInstall;
