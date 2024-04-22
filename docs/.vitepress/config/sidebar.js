2781248b74e276ed5677a23aba091f503add0ca9
vaebe <18137693952@163.com>
2024-04-22 23:12:58 +0800

    docs: 使用 vitePress 搭建文档

---
+++ docs/.vitepress/config/sidebar.js
@@ -0,0 +1,17 @@
+export const sidebar = {
+  "/guide/": [
+    {
+      text: "简介",
+      items: [
+        {
+          text: "什么是 create-neat",
+          link: "/guide/what-is-create-neat",
+        },
+        {
+          text: "快速开始",
+          link: "/guide/getting-started",
+        },
+      ],
+    },
+  ],
+};
