701056fff81b06291e8df4772ec05a0853054b25
ve <52314078+vaebe@users.noreply.github.com>
2024-04-23 09:53:09 +0800

    docs: 使用 vitePress 搭建文档 (#124)
    * docs: 使用 vitePress 搭建文档
    
    * style(docs): config.mjs 代码风格格式化
    
    * docs: 📚 版本号更新

---
+++ docs/.vitepress/config/head.js
@@ -0,0 +1,29 @@
+export const head = [
+  ["meta", { name: "theme-color", content: "#ffffff" }],
+  // ['link', { rel: 'icon', href: '/logo.png', type: 'image/png' }],
+  ["meta", { name: "author", content: "Moment" }],
+  [
+    "meta",
+    {
+      property: "og:title",
+      content: "create-neat | Zero configuration to quickly create your application",
+    },
+  ],
+  ["meta", { property: "og:type", content: "website" }],
+  ["meta", { property: "og:locale", content: "zh-CN" }],
+  ["meta", { property: "og:site_name", content: "create-neat" }],
+  [
+    "meta",
+    {
+      property: "og:description",
+      content: "基于 PNPM 和 Turborepo 开发了前端脚手架，旨在为用户快速创建各种类型的项目。",
+    },
+  ],
+  [
+    "meta",
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1.0, viewport-fit=cover",
+    },
+  ],
+];
