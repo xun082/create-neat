a6a09bd5649a126d05425779d596ad59e3cd7204
non_hana <105428118+nonhana@users.noreply.github.com>
2024-04-27 09:54:35 +0800

    feat: TemplateAPI初步实现 (#126)

--- packages/core/src/utils/select.ts
+++ packages/core/src/utils/select.ts
@@ -36,6 +36,7 @@ async function projectSelect() {
       { value: "common-lib", label: "common-lib" },
       { value: "vue", label: "vue" },
       { value: "react", label: "react" },
+      { value: "template-test", label: "test" },
     ],
   })) as string;
 
