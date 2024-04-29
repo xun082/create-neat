f524b75c752232fbc195d19ebc9515f17d0fca02
Lemonadeccc <82374719+Lemonadeccc@users.noreply.github.com>
2024-04-29 20:37:51 +0800

    feat: 🚀 preminliary implementation of plugin-eslint & remove redundant code. (#138)

---
+++ packages/@plugin/plugin-eslint/generator/index.cjs
@@ -0,0 +1,20 @@
+
+module.exports = (generatorAPI) => {
+  generatorAPI.extendPackage({
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      node: true,
+    },
+    extends: ["eslint:recommended"],
+    parserOptions: {
+      root: true,
+      parser: "@babel/eslint-parser",
+      ecmaVersion: 2021,
+      sourceType: "module",
+      requireConfigFile: false,
+    },
+    plugins: ['import', 'jsx-a11y'],
+  })
+}
