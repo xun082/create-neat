621f9fea07dc8e9ea52e3a54989197c85b3e15ea
cai-cai-hu <166509524+cai-cai-hu@users.noreply.github.com>
2024-05-04 12:11:08 +0800

    refactor: ♻️ refactor: 在packages下的core和utils增加了jsDoc (#147)

--- packages/core/src/models/TemplateAPI.ts
+++ packages/core/src/models/TemplateAPI.ts
@@ -10,9 +10,17 @@ interface ConfigFileData {
  * @param generator Generator实例
  */
 class TemplateAPI {
-  // 临时的依赖数据，需要调用 GeneratorAPI 的 extendPackage 方法将其应用到 package.json 中
+  /**
+   * 临时的依赖数据，需要调用 GeneratorAPI 的 extendPackage 方法将其应用到 package.json 中。
+   * @private
+   * @type {object}
+   */
   private packageData = { dependencies: {}, devDependencies: {}, scripts: {} };
-  // 临时的配置文件数据，需要调用 Generator 的 extendConfigFile 方法将其应用到模板中
+  /**
+   * 临时的配置文件数据，需要调用 Generator 的 extendConfigFile 方法将其应用到模板中。
+   * @private
+   * @type {Record<string, ConfigFileData>}
+   */
   private configFilesData: Record<string, ConfigFileData> = {};
   private generator: Generator;
   private generatorAPI: GeneratorAPI;
@@ -22,34 +30,61 @@ class TemplateAPI {
     this.generatorAPI = new GeneratorAPI(generator);
   }
 
-  // 添加依赖
+  /**
+   * 添加依赖。
+   * @method
+   * @param {string} name - 依赖名称。
+   * @param {string} [version="latest"] - 依赖版本。
+   */
   addDependency(name: string, version: string = "latest") {
     this.packageData.dependencies[name] = version;
   }
 
-  // 添加开发依赖
+  /**
+   * 添加开发依赖。
+   * @method
+   * @param {string} name - 依赖名称。
+   * @param {string} [version="latest"] - 依赖版本。
+   */
   addDevDependency(name: string, version: string = "latest") {
     this.packageData.devDependencies[name] = version;
   }
 
-  // 添加脚本
+  /**
+   * 添加脚本。
+   * @method
+   * @param {string} name - 脚本名称。
+   * @param {string} command - 脚本命令。
+   */
   addScript(name: string, command: string) {
     this.packageData.scripts[name] = command;
   }
 
-  // 扩展 package.json
+  /**
+   * 扩展 package.json。
+   * @method
+   * @param {object} fields - 需要扩展的字段。
+   */
   extendPackage(fields: object) {
     Object.assign(this.packageData, fields);
   }
 
-  // 扩展配置文件
+  /**
+   * 扩展配置文件。
+   * @method
+   * @param {string} fileName - 配置文件名称。
+   * @param {ConfigFileData} data - 配置文件数据。
+   */
   extendConfigFile(fileName: string, data: ConfigFileData) {
     // 如果不存在，则初始化
     if (!this.configFilesData[fileName]) this.configFilesData[fileName] = { file: {} };
     Object.assign(this.configFilesData[fileName], data);
   }
 
-  // 调用 Generator 的方法来将收集到的数据应用到模板和配置文件中
+  /**
+   * 调用 Generator 的方法来将收集到的数据应用到模板和配置文件中。
+   * @method
+   */
   generateFiles() {
     // 1. 将依赖数据应用到 package.json 中
     this.generatorAPI.extendPackage(this.packageData);
@@ -59,8 +94,11 @@ class TemplateAPI {
       this.generator.extendConfigFile(fileName, data);
     }
   }
-
-  // 提供一个获取当前收集状态的方法，供 Generator 在渲染前查询
+  /**
+   * 提供一个获取当前收集状态的方法，供 Generator 在渲染前查询。
+   * @method
+   * @returns {object} 当前收集状态的数据。
+   */
   getTemplateData() {
     return {
       packageData: this.packageData,
