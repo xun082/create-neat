a832ed3172db857bf87e819f453b42572e8bfa65
yyx <76930408+yangdaxian-is-cool@users.noreply.github.com>
2024-05-13 20:35:40 +0800

    feat: 🚀 ast合并vite配置功能 (#161)
    * feat: 🚀 ast合并vite配置功能
    
    * refactor: ♻️ 优化插件配置写入构建工具配置流程
    
    (cherry picked from commit b54228167d359e5dbe964aa2ea6f23c0ab78cffb)

--- packages/@plugin/plugin-babel/index.cjs
+++ packages/@plugin/plugin-babel/index.cjs
@@ -1,36 +1,7 @@
-const buildToolConfigs = {
-  // 支持拓展loader和plugin
-  webpack: () => {
-    return {
-      rules: [
-        {
-          test: /\.js$/, // 匹配所有以 .js 结尾的文件
-          exclude: /node_modules/, // 排除 node_modules 目录
-          use: {
-            loader: "babel-loader", // 指定 Babel Loader
-          },
-        },
-      ],
-      plugins: [{ name: "a", params: [], import: { name: "a", from: "b" } }],
-    };
-  },
-  vite: () => {
-    return {};
-  },
-  // 添加其他构建工具的配置...
-};
+const PluginConfig = require("./config/index.cjs");
 
 const pluginBabel = (buildTool) => {
-  const configHandler = buildToolConfigs[buildTool];
-
-  if (configHandler) {
-    return configHandler();
-  } else {
-    console.warn(`Unsupported build tool: ${buildTool}`);
-  }
-
-  // 其他独立于构建工具的配置
-  // ……
+  return PluginConfig[buildTool] ?? console.warn(`Unsupported build tool: ${buildTool}`);
 };
 
 module.exports = pluginBabel;
