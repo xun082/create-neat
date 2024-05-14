8b28a1e9698dab9a685c8537d07bcf174ee5eb37
Kylin <95574719+IsDyh01@users.noreply.github.com>
2024-05-14 13:09:45 +0800

    refactor: 修改生成插件配置文件方法 (#162)
    (cherry picked from commit 81ccf12954ad9056867bf66a82d26e4c7de33675)

--- packages/@plugin/plugin-eslint/generator/index.cjs
+++ packages/@plugin/plugin-eslint/generator/index.cjs
@@ -1,19 +1,21 @@
 module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      node: true,
+    eslint: {
+      env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+      },
+      extends: ["eslint:recommended"],
+      parserOptions: {
+        root: true,
+        parser: "@babel/eslint-parser",
+        ecmaVersion: 2021,
+        sourceType: "module",
+        requireConfigFile: false,
+      },
+      plugins: ["import", "jsx-a11y"],
     },
-    extends: ["eslint:recommended"],
-    parserOptions: {
-      root: true,
-      parser: "@babel/eslint-parser",
-      ecmaVersion: 2021,
-      sourceType: "module",
-      requireConfigFile: false,
-    },
-    plugins: ["import", "jsx-a11y"],
   });
 };
