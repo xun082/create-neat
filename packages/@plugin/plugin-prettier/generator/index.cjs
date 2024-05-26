f2e7bbb3d55219d2bbee94fafc013046445e2105
yuhao <1772156189@qq.com>
2024-05-26 20:47:05 +0800

    fix: 🧩 fix: 修改 prettier 配置内容并加入 scripts 脚本

--- packages/@plugin/plugin-prettier/generator/index.cjs
+++ packages/@plugin/plugin-prettier/generator/index.cjs
@@ -1,18 +1,22 @@
 module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
     prettier: {
-      singleQuote: false,
-      tabWidth: 2,
-      arrowParens: "always",
-      bracketSpacing: true,
-      proseWrap: "preserve",
-      trailingComma: "all",
-      jsxSingleQuote: false,
-      printWidth: 100,
+        tabWidth: 2,                // 每个缩进级别的空格数
+        printWidth: 80,             // 每行代码的最大长度
+        useTabs: false,             // 使用空格代替制表符进行缩进
+        semi: true,                 // 在语句末尾添加分号
+        singleQuote: true,          // 使用单引号代替双引号
+        trailingComma: "es5",       // 尾随逗号的使用方式（es5 对象、数组等使用）
+        bracketSpacing: true,       // 对象文字中的括号间是否加空格
+        arrowParens: "always",      // 箭头函数参数使用圆括号
+        endOfLine: "lf"             // 换行符使用 lf (Unix 风格)    
     },
     devDependencies: {
       "@types/prettier": "^3.0.0",
       prettier: "^3.1.0",
     },
+    scripts:{
+      "format": "prettier --write \"src/**/*.{js,jsx,ts,tsx,json,css,scss,md}\"",
+    }
   });
 };
