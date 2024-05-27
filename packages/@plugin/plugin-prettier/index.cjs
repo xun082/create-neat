8c02b235c4bff1fd65bb2edb148b7da8e4dc812c
yuhao <76569582+yuhao423@users.noreply.github.com>
2024-05-27 17:28:50 +0800

    feat: 🚀 增加 prettier 插件入口 cjs文件 (#176)
    * feat: 🚀增加 prettier 插件入口 cjs文件
    
    * fix: 🧩 修改 prettier 配置内容并加入 scripts 脚本
    
    * fix: 🧩 抽离单独 prettier 必要依赖等

---
+++ packages/@plugin/plugin-prettier/index.cjs
@@ -0,0 +1,10 @@
+
+const PluginConfig = require('./generator/index.cjs')
+
+const pluginPrettier = (buildTool)=>{
+  return PluginConfig[buildTool] ?? console.warn(`Unsupported build tool: ${buildTool}`);
+}
+
+module.exports = {
+    pluginPrettier
+}
