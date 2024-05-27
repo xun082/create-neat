4300c81123e2b97afb07b3d0be02bed80adb0996
wuwenbang <wuwenbang@foxmail.com>
2024-05-27 23:01:17 +0800

    chore: 添加 React 依赖项

--- packages/core/template/template-react/generator/index.cjs
+++ packages/core/template/template-react/generator/index.cjs
@@ -1,6 +1,9 @@
 module.exports = (templateAPI) => {
   templateAPI.extendPackage({
-    dependencies: {},
+    dependencies: {
+      "react": "^18.2.0",
+      "react-dom": "^18.2.0"
+    },
     devDependencies: {},
   });
 };
