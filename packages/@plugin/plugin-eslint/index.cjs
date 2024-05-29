b02069896e0497ff6225e1bc8bc3474fbf340d99
yuhao <76569582+yuhao423@users.noreply.github.com>
2024-05-29 21:20:26 +0800

    fix: 🧩 修复 eslint 的依赖错误 (#185)
    * feat: 🚀 feat: 增加 prettier 插件入口 cjs文件
    
    * fix: 🧩 fix: 修改 prettier 配置内容并加入 scripts 脚本
    
    * fix: 🧩 fix: 抽离单独 prettier 必要依赖等
    
    * fix: 🧩 修复 eslint 的依赖错误
    
    (cherry picked from commit d2341d4e28ba4743c771b95ff2cc55206b3af9f1)

--- packages/@plugin/plugin-eslint/index.cjs
+++ packages/@plugin/plugin-eslint/index.cjs
@@ -1,36 +1,7 @@
-const buildToolConfigs = {
-  webpack: (config) => {
-    if (config.resolve.alias) {
-      config.resolve.alias.set("a", "b");
-    } else {
-      config.resolve.alias = {
-        a: "b",
-      };
-    }
-  },
-  vite: (options) => {
-    options.alias = [
-      {
-        find: "a",
-        replacement: "b",
-      },
-    ];
-  },
-  // 添加其他构建工具的配置...
-};
+const PluginConfig = require('./generator/index.cjs')
 
-const pluginEslint = (api, options) => {
-  const { buildTool } = options.preset;
-  const configHandler = buildToolConfigs[buildTool];
-
-  if (configHandler) {
-    configHandler(api, options);
-  } else {
-    console.warn(`Unsupported build tool: ${buildTool}`);
-  }
-
-  // 其他独立于构建工具的配置
-  // ……
+const pluginEslint = (buildTool) => {
+  return PluginConfig[buildTool] ?? console.warn(`Unsupported build tool: ${buildTool}`);
 };
 
 module.exports = {
