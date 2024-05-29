bdfb3a1f466a50945f608ce607ad21715f903b77
cmtlyt <cmtlyt@163.com>
2024-05-29 21:44:57 +0800

    style: 🎨 格式化

--- packages/@plugin/plugin-prettier/index.cjs
+++ packages/@plugin/plugin-prettier/index.cjs
@@ -1,10 +1,9 @@
+const PluginConfig = require("./generator/index.cjs");
 
-const PluginConfig = require('./generator/index.cjs')
-
-const pluginPrettier = (buildTool)=>{
+const pluginPrettier = (buildTool) => {
   return PluginConfig[buildTool] ?? console.warn(`Unsupported build tool: ${buildTool}`);
-}
+};
 
 module.exports = {
-    pluginPrettier
-}
+  pluginPrettier,
+};
