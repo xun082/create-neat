ba9c9ee4aa4169227b0f9f2bc3e519e2a3783230
yuhao <1772156189@qq.com>
2024-06-04 10:04:36 +0800

    fix: 🧩 修复冲突

--- packages/@plugin/plugin-prettier/generator/index.cjs
+++ packages/@plugin/plugin-prettier/generator/index.cjs
@@ -1,17 +1,17 @@
 module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
     prettier: {
-        tabWidth: 2,                // 每个缩进级别的空格数
-        printWidth: 80,             // 每行代码的最大长度
-        useTabs: false,             // 使用空格代替制表符进行缩进
-        semi: true,                 // 在语句末尾添加分号
-        singleQuote: true,          // 使用单引号代替双引号  
+      tabWidth: 2, // 每个缩进级别的空格数
+      printWidth: 80, // 每行代码的最大长度
+      useTabs: false, // 使用空格代替制表符进行缩进
+      semi: true, // 在语句末尾添加分号
+      singleQuote: true, // 使用单引号代替双引号
     },
     devDependencies: {
-      "prettier": "^3.1.0",
+      prettier: "^3.1.0",
     },
-    scripts:{
-      "format": "prettier --write \"src/**/*.{js,jsx,ts,tsx,json,css,scss,md}\"",
-    }
+    scripts: {
+      format: 'prettier --write "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}"',
+    },
   });
 };
