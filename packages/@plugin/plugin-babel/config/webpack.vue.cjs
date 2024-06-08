d8abc5850fa40890b018289b91a4130ec7f2ee14
xun082 <2042204285@qq.com>
2024-06-08 11:44:36 +0800

    feat: 🚀 添加 webpack 完整配置,并添加 scripts 脚本用于启动 webpack 编译

---
+++ packages/@plugin/plugin-babel/config/webpack.vue.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  rules: [
+    {
+      test: /\.vue$/,
+      use: [
+        {
+          loader: "vue-loader",
+          options: {
+            compilerOptions: {
+              preserveWhitespace: false,
+            },
+            hotReload: isDevelopment,
+          },
+        },
+      ],
+    },
+    {
+      test: /\.(tsx?|jsx?)$/,
+      include: [path.resolve(__dirname, "./src")],
+      exclude: [/node_modules/, /public/, /(.|_)min\.js$/],
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            babelrc: true,
+            cacheDirectory: true,
+            cacheCompression: false,
+          },
+        },
+        require.resolve("@ant-design-vue/vue-jsx-hot-loader"),
+      ],
+    },
+  ],
+  plugins: [],
+};
