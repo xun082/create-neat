8972c072c34f7f9f24dd29a0adc0d247276882d0
Moment <73689580+xun082@users.noreply.github.com>
2024-06-09 22:34:33 +0800

    fix: 🧩 修复 webpack 配置错误,以及require.resolve引起的一些模块加载错误 (#203)
    * fix: 🧩 修复 webpack 配置错误,以及require.resolve引起的一些模块加载错误
    
    * fix: 🧩 修复 webpack 配置错误,以及require.resolve引起的一些模块加载错误
    
    * fix: 🧩 修复 webpack 配置错误,以及require.resolve引起的一些模块加载错误
    
    (cherry picked from commit abc5e86a15a48129b8747793e388cb940ae6f381)

--- packages/@plugin/plugin-eslint/generator/index.cjs
+++ packages/@plugin/plugin-eslint/generator/index.cjs
@@ -2,26 +2,26 @@ module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
     eslint: {
       env: {
-        "browser": true,
-        "es2021": true,
-        "node": true
+        browser: true,
+        es2021: true,
+        node: true,
       },
       extends: "eslint:recommended",
       parserOptions: {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        ecmaVersion: 12,
+        sourceType: "module",
       },
       rules: {
-        "indent": ["error", 2],
+        indent: ["error", 2],
         "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "always"],
+        quotes: ["error", "single"],
+        semi: ["error", "always"],
         "no-console": "warn",
-        "no-unused-vars": "warn"
-      }
+        "no-unused-vars": "warn",
+      },
     },
-    scripts:{
-      "lint": "lint .",
-    }
+    scripts: {
+      lint: "lint .",
+    },
   });
 };
