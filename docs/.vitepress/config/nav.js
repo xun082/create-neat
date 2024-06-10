f0655f935f07798ab26b76b7823ffa3e07bd7eda
Moment <73689580+xun082@users.noreply.github.com>
2024-06-10 23:21:16 +0800

    refactor: ♻️ 删除没有使用到的项目文件 (#207)
    * refactor: ♻️ 删除没有使用到的项目文件

--- docs/.vitepress/config/nav.js
+++
@@ -1,6 +0,0 @@
-export const nav = [
-  { text: "首页", link: "/" },
-  { text: "指南", activeMatch: `^/guide/`, link: "/guide/what-is-create-neat" },
-  { text: "贡献指南", link: "/CONTRIBUTING" },
-  { text: "贡献者", link: "/contributors" },
-];
