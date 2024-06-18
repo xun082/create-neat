3878176a9c1edf9e5877ad600ffb9bbe9d29e731
15439 <1543922073@qq.com>
2024-06-18 23:00:51 +0800

    fix: 🧩 删除多余配置

--- packages/@plugin/plugin-husky/generator/index.cjs
+++ packages/@plugin/plugin-husky/generator/index.cjs
@@ -1,24 +1,14 @@
 module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
     scripts: {
-      prepare: "husky",
-      commit: "git-cz",
-      format: 'prettier --write "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}"',
-      lint: 'eslint "**/*.{ts,tsx,js,jsx}" --fix',
-    },
-    config: {
-      commitizen: {
-        path: "node_modules/cz-git",
-      },
+      postinstall: "husky install",
     },
     "lint-staged": {
-      "*.{ts,tsx,js,jsx}": ["npm run format", "npm run lint"],
+      "*.{ts,tsx,js,jsx}": ["pnpm format:ci", "pnpm lint:ci"],
     },
     devDependencies: {
       husky: "^9.0.11",
       "lint-staged": "^15.2.0",
-      "cz-git": "^1.7.1",
-      commitizen: "^4.3.0",
       "@commitlint/cli": "^18.4.3",
       "@commitlint/config-conventional": "^18.4.3",
     },
