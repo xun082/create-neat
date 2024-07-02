c63adbf94b14a4c6c7761ccd2e4a7c728c349301
Moment <73689580+xun082@users.noreply.github.com>
2024-07-02 20:43:36 +0800

    fix: 🧩 修复启动webpack时报错 (#230)
    (cherry picked from commit 9a4b82564c1a3aa0c338ba7d5ab2f3e1660a7afc)

--- packages/@plugin/plugin-mobx/generator/index.cjs
+++ packages/@plugin/plugin-mobx/generator/index.cjs
@@ -2,8 +2,7 @@ module.exports = (generatorAPI) => {
   generatorAPI.extendPackage({
     devDependencies: {
       mobx: "^6.6.4",
-      "mobx-react-lite":"^3.2.2",
+      "mobx-react-lite": "^3.2.2",
     },
   });
 };
- 
