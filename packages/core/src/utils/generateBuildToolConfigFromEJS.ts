57c53dd0694207b3ae503e5376f240fff95fc4f4
sp <2849872658@qq.com>
2024-09-20 11:59:32 +0800

    fix: 🧩 修复导出文件名称错误

--- packages/core/src/utils/generateBuildToolConfigFromEJS.ts
+++ packages/core/src/utils/generateBuildToolConfigFromEJS.ts
@@ -1,6 +1,6 @@
 import ejs from "ejs";
 
-export default function generateWebpackConfigFromEJS(options, template: string) {
+export default function generateBuildToolConfigFromEJS(options, template: string) {
   const config = ejs.render(template, options);
 
   return config;
