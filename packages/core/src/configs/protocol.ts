c50d8b99fb35bc13c88d645ed1dbb9cd1538460f
maofeixiang04 <14276833+maofeixiang04@user.noreply.gitee.com>
2024-10-15 19:00:02 +0800

    test

--- packages/core/src/configs/protocol.ts
+++ packages/core/src/configs/protocol.ts
@@ -1,6 +1,90 @@
+// 通用协议
+export const commonProtocol = {
+  ENTRY_FILE: [{
+    pluginName: 'scss',
+    version: '',
+    affects: {
+      template: {
+        description: '影响框架入口文件配置',
+        changes: {
+          description: '入口引入全局scss文件',
+          content:'import ./style/index.scss'
+        }
+      }
+    }
+  },
+  {
+    pluginName: 'element-plus',
+    version: '',
+    affects: {
+      template: {
+        description: '影响框架入口文件配置',
+        changes: {
+          description: '入口引入全局element-plus',
+          content: "import ElementPlus from 'element-plus' import 'element-plus/dist/index.css' "
+        }
+      }
+    }
+},{
+  pluginName: 'Pinia',
+  version: '',
+  affects: {
+    template: {
+      description: '影响框架入口文件配置',
+      changes: [
+        {
+          description: '入口引入全局Pinia',
+          content: "import { createPinia } from 'pinia' "
+        },
+        {
+          description: '创建Pinia实例',
+          content: "const Pinia=createPinia()"
+        }
+      ]
+    }
+  }
+}]
+}
 // 插件对框架的协议
 export const pluginToTemplateProtocol = {
-  ENTRY_FILE: "ENTRY_FILE", // 入口文件配置，如：入口文件引入全局 less、scss
+  INSERT_STYLE: [{
+    pluginName: 'scss',
+    version: '',
+    affects: {
+      template: {
+        description: '影响框架根组件文件配置',
+        changes: {
+          description: '根组件引入scss',
+          content:'<style lang="scss">'
+        }
+      }
+    }
+  }],
+  INSERT_ROOTFILE: [{
+    pluginName: 'Pinia',
+    version: '',
+    affects: {
+      template: {
+        description: '影响框架根入口文件配置',
+        changes: {
+          description: '增加usePinia',
+          content:'app.use(Pinia)'
+        }
+      }
+    }
+  }, {
+    pluginName: 'element-plus',
+    version: '',
+    affects: {
+      template: {
+        description: '影响框架根入口文件配置',
+        changes: {
+          description: '增加useElementPlus',
+          content:'app.use(ElementPlus)'
+        }
+      }
+    }
+  }]
   // ……
 };
 
