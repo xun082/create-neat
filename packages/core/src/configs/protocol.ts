88b60ab9d2fcfa2c302eef317d0b6d9e3153e484
maofeixiang04 <14276833+maofeixiang04@user.noreply.gitee.com>
2024-10-26 21:48:24 +0800

    feat: Pinia ElementPlus插件对应协议

--- packages/core/src/configs/protocol.ts
+++ packages/core/src/configs/protocol.ts
@@ -1,90 +1,101 @@
 // 通用协议
 export const commonProtocol = {
-  ENTRY_FILE: [{
-    pluginName: 'scss',
-    version: '',
-    affects: {
-      template: {
-        description: '影响框架入口文件配置',
-        changes: {
-          description: '入口引入全局scss文件',
-          content:'import ./style/index.scss'
-        }
-      }
-    }
-  },
-  {
-    pluginName: 'element-plus',
-    version: '',
-    affects: {
-      template: {
-        description: '影响框架入口文件配置',
-        changes: {
-          description: '入口引入全局element-plus',
-          content: "import ElementPlus from 'element-plus' import 'element-plus/dist/index.css' "
-        }
-      }
-    }
-},{
-  pluginName: 'Pinia',
-  version: '',
-  affects: {
-    template: {
-      description: '影响框架入口文件配置',
-      changes: [
-        {
-          description: '入口引入全局Pinia',
-          content: "import { createPinia } from 'pinia' "
+  ENTRY_FILE: [
+    {
+      pluginName: "scss",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架入口文件配置",
+          changes: {
+            description: "入口引入全局scss文件",
+            content: "import './style/index.scss'",
+          },
         },
-        {
-          description: '创建Pinia实例',
-          content: "const Pinia=createPinia()"
-        }
-      ]
-    }
-  }
-}]
-}
+      },
+    },
+    {
+      pluginName: "element-plus",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架入口文件配置",
+          changes: {
+            description: "入口引入全局element-plus",
+            content: `
+  import ElementPlus from 'element-plus'
+  import 'element-plus/dist/index.css'
+`,
+          },
+        },
+      },
+    },
+    {
+      pluginName: "Pinia",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架入口文件配置",
+          changes: {
+            description: "入口引入全局Pinia",
+            content: "import { createPinia } from 'pinia' ",
+          },
+        },
+      },
+    },
+  ],
+};
 // 插件对框架的协议
 export const pluginToTemplateProtocol = {
-  INSERT_STYLE: [{
-    pluginName: 'scss',
-    version: '',
-    affects: {
-      template: {
-        description: '影响框架根组件文件配置',
-        changes: {
-          description: '根组件引入scss',
-          content:'<style lang="scss">'
-        }
-      }
-    }
-  }],
-  INSERT_ROOTFILE: [{
-    pluginName: 'Pinia',
-    version: '',
-    affects: {
-      template: {
-        description: '影响框架根入口文件配置',
-        changes: {
-          description: '增加usePinia',
-          content:'app.use(Pinia)'
-        }
-      }
-    }
-  }, {
-    pluginName: 'element-plus',
-    version: '',
-    affects: {
-      template: {
-        description: '影响框架根入口文件配置',
-        changes: {
-          description: '增加useElementPlus',
-          content:'app.use(ElementPlus)'
-        }
-      }
-    }
-  }]
+  INSERT_STYLE: [
+    {
+      pluginName: "scss",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架根组件文件配置",
+          changes: {
+            description: "根组件引入scss",
+            content: '<style lang="scss">',
+          },
+        },
+      },
+    },
+  ],
+  INSERT_ROOTFILE: [
+    {
+      pluginName: "Pinia",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架根入口文件配置",
+          changes: [
+            {
+              description: "增加usePinia",
+              content: "app.use(Pinia)",
+            },
+            {
+              description: "创建Pinia实例",
+              content: "const Pinia=createPinia()",
+            },
+          ],
+        },
+      },
+    },
+    {
+      pluginName: "element-plus",
+      version: "@latest",
+      affects: {
+        template: {
+          description: "影响框架根入口文件配置",
+          changes: {
+            description: "增加useElementPlus",
+            content: "app.use(ElementPlus)",
+          },
+        },
+      },
+    },
+  ],
   // ……
 };
 
