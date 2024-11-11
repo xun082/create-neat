a8b701d2e13581e9dc673a62a4aa69f126817272
Ddzd20 <2387155220@qq.com>
2024-11-11 11:16:40 +0800

    fix: 修改 content,增加参数类型

--- packages/core/src/configs/protocol.ts
+++ packages/core/src/configs/protocol.ts
@@ -1,14 +1,18 @@
+const globalProtocol = {
+  ENTRY_FILE: "ENTRY_FILE", // 入口文件配置，如：入口文件引入全局 less、scss
+}
+
 // 插件对框架的协议
 exports.pluginToTemplateProtocol = {
-  ENTRY_FILE: "ENTRY_FILE", // 入口文件配置，如：入口文件引入全局 less、scss
-  // ……
+  ...globalProtocol,
 };
 
 // 插件对构建工具的协议
-exports.pluginToBuildToolProtocol = {};
+exports.pluginToBuildToolProtocol = {
+  ...globalProtocol,
+};
 // 框架对构建工具的协议
 exports.templateToBuildToolProtocol = {
-  ENTRY_FILE: "ENTRY_FILE", //入口文件配置，比如根据不同框架引入不同方法
+  ...globalProtocol,
   ADD_CONFIG: "ADD_CONFIG", //根据框架，不同的打包工具需要不同的插件，有些是都需要用的，有些是框架独有的
-  ADD_DEPENDENCIES: "ADD_DEPENDENCIES", //框架所需要的依赖，已有现成方法，可以直接调用
 };
