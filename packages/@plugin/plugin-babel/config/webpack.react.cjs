a7c600f0ca9a31a8342154bb0be6657f183b1fba
18003228289 <1440203070@qq.com>
2024-11-19 21:34:57 +0800

    feat:实现插件对构建工具协议

--- packages/@plugin/plugin-babel/config/webpack.react.cjs
+++ packages/@plugin/plugin-babel/config/webpack.react.cjs
@@ -3,12 +3,11 @@ const path = require("path");
 module.exports = {
   rules: [
     {
-      test: /\.(js|jsx|ts|tsx)$/, // Babel 处理 .js, .jsx, .ts, .tsx 文件
-      exclude: /node_modules/, // 排除 node_modules 目录
+      test: /\.(js|jsx|ts|tsx)$/, 
+      exclude: /node_modules/, 
       use: {
         loader: "babel-loader",
         options: {
-          // 使用 babel.config.js 配置
           configFile: path.resolve(__dirname, "babel.config.js"),
         },
       },
